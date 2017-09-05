aaad99c651bd62078e5c2e94a24debdfecba3369
DMonMac <dmmacaso@gmail.com>
2017-09-06 05:38:52 +0800

    Tranferred files.

--- ATM-App/index.js
+++ ATM-App/index.js
@@ -1,46 +1,109 @@
-// Essentials
-// Express
-var express = require('express');
-var app = express();
+const express = require('express');
+const redirect = require('express-redirect');
+const path = require('path');
+const bodyParser = require('body-parser')
+const app = express();
+var cookieParser = require('cookie-parser')
+app.use(cookieParser())
+redirect(app);
 
-// Parser
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var upload = multer();
-
-// Set view engine and folder
-app.set('view engine', 'pug');
-app.set('views','./views');
-
-// for parsing application/json
-app.use(bodyParser.json());
-// for parsing application/xwww-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// for parsing multipart/form-data
-app.use(upload.array());
+var users = [
+	{'id': 1, 'email' : 'dmac@email.com', 'pass' : 'Thisis@test', 'money' : 1000000},
+	{'id': 2, 'email' : 'macd@email.com', 'pass' : 'Thisis@test', 'money' : 9999999},
+	{'id': 3, 'email' : 'failbeing@email.com', 'pass' : 'Thisis@test2', 'money' : 100}
+	];
 
 
-// Mock 'client database'
-var clients = [
-  {first_name:"D", last_name:"Mac", email:"dmac@email.com", password:"Thisis@test", bal: 1000000},
-  {first_name:"Mac", last_name:"D", email:"macd@email.com", password:"Thisis@test2", bal: 9999999},
-  {first_name:"Fail", last_name:"Being", email:"failbeing@email.com", password:"Thisis@test3", bal: 100}
-];
+// Function for parsing html files
+function myHTML(url) {
+	return path.join(__dirname+'/views/'+url+'.html');
+}
 
+// Tell express to use the body-parser middleware and to not parse extended bodies
+app.use(bodyParser.urlencoded({ extended: false }))
 
-// Routes
-app.get('/', function(req, res){
-   res.render("home.pug");
+// Routing for HomePage
+app.get('/', function(req, res, next){
+		var cookie = req.cookies.logged;
+		console.log(cookie);
+		res.sendFile(myHTML('index'));
+  		console.log('Displaying Home Page');
 });
 
-
-// POST actions
-app.post('/', function(req, res){
-   console.log(req.body);
-   res.send("Received input :)");
+// Routing for HomePage
+app.get('/api/:id', function(req, res, next){
+    for (var i=0; i<users.length; i++) {
+      if (users[i].id == req.params.id) {
+        res.send(users[i]);
+        break;
+      }
+    }
+    res.send('User not Found')
 });
 
+// Routing for Login
+app.post('/login', function (req, res) {
+  var user = req.body.email;
+  var pass = req.body.password;
+  console.log(`Username: ${user}, Password: ${pass}`);
+  users.map(function(u){
+  	for (var i=0; i<users.length; i++) {
+  		if (users[i].email == user && users[i].pass == pass) {
+  			'Login Success'
+            res.cookie('logged',users[i].email);
+            console.log('cookie have created successfully');
+            res.cookie('money',users[i].money);
+            console.log('cookie have created successfully');
+            cookie = req.cookies.logged;
+  			res.redirect('/member');
+  			break;
+  		}
+  	}
+  });
 
-// In browser, type 'http://localhost:8080'
-app.listen(8080);
+});
+
+// Routing for Withdraw
+app.post('/withdraw', function (req, res) {
+  var amount = parseInt(req.body.amount);
+  var cur_money = parseInt(req.cookies.money);
+  var new_money = cur_money - amount;
+  res.cookie('money',new_money);
+  res.redirect('/member');
+});
+
+// Routing for Deposit
+app.post('/deposit', function (req, res) {
+  var amount2 = parseInt(req.body.amount2);
+  var cur_money = parseInt(req.cookies.money);
+  var new_money = cur_money + amount2;
+  res.cookie('money',new_money);
+  res.redirect('/member');
+});
+
+app.post('/logout', function (req, res) {
+
+  res.clearCookie("money");
+  res.clearCookie("logged");
+  res.redirect('/member');
+});
+
+// Routing to Member's Page
+app.get('/member', function(req, res, next){
+	cookie = req.cookies.logged;
+	money = req.cookies.money;
+	if (cookie === undefined) {
+		console.log('Not logged in... Redirecting');
+		res.redirect('/')
+	}
+	else {
+		console.log('Money: ' + money);
+		console.log('Logged in as'+cookie);
+		res.sendFile(myHTML('member'));
+  		console.log('Displaying Member Page');
+	}
+});
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000!');
+});
--- Express-Tutorial/index.js
+++ Express-Tutorial/index.js
@@ -1,67 +1,105 @@
+// Essentials
+// Express
 var express = require('express');
+var app = express();
+
+// Form Parsers
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer();
-var app = express();
+// for parsing application/xwww-form-urlencoded:
+//Tell express to use the body-parser middleware and to not parse extended bodies
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// mock "database"
-var clients = [
-  {first_name:"D", last_name:"Mac", email:"dmac@email.com", password:"Thisis@test", bal: 1000000},
-  {first_name:"Mac", last_name:"D", email:"macd@email.com", password:"Thisis@test2", bal: 9999999},
-  {first_name:"Fail", last_name:"Being", email:"failbeing@email.com", password:"Thisis@test3", bal: 100}
-];
+//Sessions
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+//var session = require('express-session');
+//app.use(session({secret: "Your secret key"}));
 
-// Setting view engine and folder
+
+// Set view engine and folder
 app.set('view engine', 'pug');
 app.set('views','./views');
 
-// GET methods
-app.get('/', function(req, res){
+redirect(app);
+
+// Mock 'client database'
+var clients = [
+  {first_name:"D", last_name:"Mac", email:"dmac@email.com", password:"Thisis@test", bal: 1000000, id: 1},
+  {first_name:"Mac", last_name:"D", email:"macd@email.com", password:"Thisis@test2", bal: 9999999, id: 2},
+  {first_name:"Fail", last_name:"Being", email:"failbeing@email.com", password:"Thisis@test3", bal: 100, id: 3}
+];
+
+
+// Routes
+app.get('/', function(req, res, next){
+  var cookie = req.cookies.logged;
+  console.log(cookie);
   res.render('home');
+  console.log('Displaying Home Page');
 });
 
-app.get('/client', function(req, res){
-  res.render('client_page');
-});
-
-app.get('/form', function(req, res){
-  res.render('form');
-});
-
-// for parsing application/json
-app.use(bodyParser.json());
-
-// for parsing application/xwww-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// for parsing multipart/form-data
-app.use(upload.array());
-app.use(express.static('public'));
-
-app.post('/', function(req, res){
-   console.log(req.body);
-   res.send("recieved your request!");
+// Routing for HomePage
+app.get('/api/:id', function(req, res, next){
+    for (var i=0; i<users.length; i++) {
+      if (users[i].id == req.params.id) {
+        res.send(users[i]);
+        break;
+      }
+    }
+    res.send('User not Found')
 });
 
 
+// POST actions
+function checkClient(req, res){
+   if(req.session.client) {
+      next();     //If session exists, proceed to page
+   } else {
+      var err = new Error("Not logged in!");
+      console.log(req.session.client);
+      next(err);  //Error, trying to access unauthorized page!
+   }
+ }
 
-// Just gonna comment these out coz they still hold valuable info
-//app.get('/first_template', function(req, res){
-//   res.render('first_view');
-//});
+app.get('/client', checkClient, function(req, res){
+  res.render('client_page', {id: req.session.client.id});
+});
 
-//app.get('/dynamic_view', function(req, res){
-//   res.render('dynamic', {
-//      name: "Tutorials Point",
-//      url:"http://www.tutorialspoint.com"
-//   });
-//});
+app.get('/log_in', function(req, res){
+  res.render('log_in');
+});
 
-//app.get('/components', function(req, res){
-//  res.render('content');
-//});
+app.post('/log_in', function(req, res){
+  console.log(clients);
+  if (!req.body.email || !req.body.password) {
+    res.render('log_in'), {message: "Please enter email and password"};
+  } else {
+      clients.filter(function(client){
+          if (client.email == req.body.email && client.password == req.body.password) {
+            req.session.client = client;
+            res.redirect('/client');
+          } else {
+              res.render('log_in', {message: "Not an account!"});
+          };
+      });
+    };
+  });
 
-//app.use('/static', express.static('public'));
-//app.use(express.static('images'));
+app.get('/logout', function(req, res){
+   req.session.destroy(function(){
+      console.log("Client logged out.")
+   });
+   res.redirect('/log_in');
+});
 
-app.listen(3000);
+app.use('/client', function(err, req, res, next){
+  console.log(err);
+  //Client should be authenticated! Redirect to log in.
+   res.redirect('/log_in');
+});
+
+
+// In browser, type 'http://localhost:8080'
+app.listen(8080);
