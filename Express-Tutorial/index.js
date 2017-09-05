58e63484ce7baf1ccec384aef8576f8f0766383f
DMonMac <dmmacaso@gmail.com>
2017-09-05 11:10:42 +0800

    installed body-parser and multer

--- Express-Tutorial/index.js
+++ Express-Tutorial/index.js
@@ -1,25 +1,50 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
 var app = express();
+
+app.get('/', function(req, res){
+  res.render('form');
+});
 
 app.set('view engine', 'pug');
 app.set('views','./views');
 
-app.get('/first_template', function(req, res){
-   res.render('first_view');
+// for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// for parsing multipart/form-data
+app.use(upload.array());
+app.use(express.static('public'));
+
+app.post('/', function(req, res){
+   console.log(req.body);
+   res.send("recieved your request!");
 });
 
-app.get('/dynamic_view', function(req, res){
-   res.render('dynamic', {
-      name: "Tutorials Point",
-      url:"http://www.tutorialspoint.com"
-   });
-});
 
-app.get('/components', function(req, res){
-  res.render('content');
-});
 
-app.use('/static', express.static('public'));
-app.use(express.static('images'));
+// Just gonna comment these out coz they still hold valuable info
+//app.get('/first_template', function(req, res){
+//   res.render('first_view');
+//});
+
+//app.get('/dynamic_view', function(req, res){
+//   res.render('dynamic', {
+//      name: "Tutorials Point",
+//      url:"http://www.tutorialspoint.com"
+//   });
+//});
+
+//app.get('/components', function(req, res){
+//  res.render('content');
+//});
+
+//app.use('/static', express.static('public'));
+//app.use(express.static('images'));
 
 app.listen(3000);
