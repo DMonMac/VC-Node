b38d5fb026b87cd52892146cd2f371bfda3b66c8
DMonMac <dmmacaso@gmail.com>
2017-09-06 15:41:25 +0800

    post action working but not saving

--- ListingLists/routes/index.js
+++ ListingLists/routes/index.js
@@ -1,12 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+// Database
+var mongoose = require('mongoose');
+var promise = mongoose.connect('mongodb://localhost/my_db', {
+  useMongoClient: true,
+});
+promise.then(function(db) {
+  /* Use `db`, for instance `db.model()`
+});
+// Or, if you already have a connection
+connection.openUri('mongodb://localhost/myapp', { /* options */ });
+
+// Create a schema
+var listingSchema = mongoose.Schema({
+   name: String
+});
+var Listing = mongoose.model('Listing', listingSchema);
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'List' });
 });
 
-router.post('/', function(req, res, next) {
+
+router.post('/', function(req, res){
+   var listingInfo = req.body; //Get the parsed information
+
+   var newListing = new Listing({
+     name: listingInfo.name
+   });
+
+   newListing.save(function(Listing){
+     res.render('show_message', {
+       message: "New list added [index]", type: "success", listing: listingInfo
+     });
+   });
+
 });
 
 module.exports = router;
